0763dd44ce82649404d898999506198485974973
Neeraj Kumar <nkurmbanshi@yahoo.com>
2024-03-06 11:20:07 +0530

    First commit

---
+++ project/urls.py
@@ -0,0 +1,33 @@
+"""project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+from app.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("",RegisterPage,name="registerpage"),
+    path("register/",UserRegister,name="register"),
+    path("loginpage/",LoginPage,name="loginpage"),
+    path("loginuser/",LoginUser,name="login"),
+    path("insert/<str:pk>",query,name="query"),
+    path('showpage/<str:pk>',showdata,name='showdata'),
+    path('editpage/<int:pk>/',editPage,name='editpage'),
+    path('update/<int:pk>/',updateData,name='update'),
+    path('delete/<int:pk>/',deleteData,name='delete'),
+    path('search/<str:pk>',search,name='search'),
+    path('logout/',logout,name='logout')
+]
